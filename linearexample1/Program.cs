fb6ceafafbebbc0404dedb9297b17abc54f1dfae
Furji UMY <furji.ft21@mail.umy.ac.id>
2022-10-27 16:38:31 +0700

    add class binarysearch

--- linearexample1/Program.cs
+++ linearexample1/Program.cs
@@ -32,7 +32,53 @@ namespace linearexample1
             Console.WriteLine("---------------------");
             Console.WriteLine(" Enter Array Element ");
             Console.WriteLine("---------------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = int.Parse(s1);
+            }
               
+        }
+
+        public void BinarySearch()
+        {
+            char ch;
+            do
+            {
+                // accept the number to be searched
+                Console.Write("\nEnter the element you want to seacrh :");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                //apply binary seacrh
+                int lowerbound = 0;
+                int upperbound = 0;
+
+                //obtain the index of the middle element
+                int mid = (lowerbound + upperbound) / 2;
+                int ctr = 1;
+
+                // loop to search for the element in the array
+                while ((item != arr[mid]) && (ctr <= upperbound))
+                {
+                    if (item == arr[mid])
+                        lowerbound = mid + 1;
+                    else
+                        upperbound = mid - 1;
+
+                    mid = (lowerbound + upperbound) / 2;
+                    ctr++;
+                }
+                if (item == arr[mid])
+                    Console.WriteLine("\n" + item.ToString() + "found at position " + (mid + 1).ToString());
+                else
+                    Console.WriteLine("\n" + item.ToString() + "not found in the array\n");
+                Console.WriteLine("\nNumber of comparasion : " + ctr);
+
+                Console.Write("\nContinue search (y/n): ");
+                ch = char.Parse(Console.ReadLine().ToUpper());
+
+            } while ((ch == 'y'));
         }
         public void Main(string[] args)
 
